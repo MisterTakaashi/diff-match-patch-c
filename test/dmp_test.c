5891294cca1af93c21508f9c7142f012e267fbab
Paul <soapmctravich@gmail.com>
2014-04-29 17:41:30 +0200

    Avec deux fichiers
    Fonctionne avec deux fichiers donnés dans le code

--- test/dmp_test.c
+++ test/dmp_test.c
@@ -3,16 +3,27 @@
 int main(int argc, char **argv)
 {
     dmp_diff *diff;
+    
+    char texte1[254];
+    char texte2[254];
+    
     FILE* fichier = NULL;
-    fichier = fopen("test.txt", "w");
+    fichier = fopen("test.txt", "r");
+    
+    FILE* fichiercomp = NULL;
+    fichiercomp = fopen("test2.txt", "r"); 
+    
+    fread(texte1, sizeof(texte1), 1, fichier);
+    fread(texte2, sizeof(texte2), 1, fichiercomp);
     
     printf("Diff en cours...\n");
     
-    dmp_diff_from_strs(&diff, NULL, "Apples are a fruit.", "Bananas are also fruit.");
-    dmp_diff_print_raw(fichier, diff);
+    //dmp_diff_from_strs(&diff, NULL, "Apples are a fruit.", "Bananas are also fruit.");
+    dmp_diff_new(&diff, NULL, texte1, strlen(texte1), texte2, strlen(texte2));
+    dmp_diff_print_raw(stderr, diff);
 	dmp_diff_free(diff);
     
-    printf("\n\nDiff en cours...\n");
+    printf("\nDiff en cours...\n");
     
     return 0;
 }
