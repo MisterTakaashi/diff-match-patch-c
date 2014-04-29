3fbed3fc531b12c51939ee2521ff0cd17fa69af0
Paul <soapmctravich@gmail.com>
2014-04-29 16:45:50 +0200

    Differences appliquées dans un fichier

--- test/dmp_test.c
+++ test/dmp_test.c
@@ -3,11 +3,16 @@
 int main(int argc, char **argv)
 {
     dmp_diff *diff;
+    FILE* fichier = NULL;
+    fichier = fopen("test.txt", "w");
+    
     printf("Diff en cours...\n");
     
     dmp_diff_from_strs(&diff, NULL, "Apples are a fruit.", "Bananas are also fruit.");
-    dmp_diff_print_raw(stderr, diff);
+    dmp_diff_print_raw(fichier, diff);
 	dmp_diff_free(diff);
+    
+    printf("\n\nDiff en cours...\n");
     
     return 0;
 }
