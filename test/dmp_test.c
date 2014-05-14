1f95edcfc92e61dffb104f325daab0bc8db98bc3
Paul <soapmctravich@gmail.com>
2014-05-14 15:35:20 +0200

    Maximum: 75 cracteres

--- test/dmp_test.c
+++ test/dmp_test.c
@@ -16,13 +16,13 @@ int main(int argc, char **argv)
 
     printf("Diff en cours...\n");
     
-    fgets(texte1, 100, fichier);
-    fgets(texte2, 100, fichiercomp);
+    //fgets(texte1, 100, fichier);
+    //fgets(texte2, 100, fichiercomp);
     
-    //printf("\n%s\n\n", texte1);
+    fread(texte1, sizeof(char)*75, 1, fichier);
+    fread(texte2, sizeof(char)*75, 1, fichiercomp);//sizeof(texte2)
     
-    //fread(texte1, sizeof(texte1), 1, fichier);
-    //fread(texte2, sizeof(texte2), 1, fichiercomp);
+    // printf("\n%s\n\n", texte1);
     
     //dmp_diff_from_strs(&diff, NULL, "Apples are a fruit.", "Bananas are also fruit.");
     dmp_diff_new(&diff, NULL, texte1, strlen(texte1), texte2, strlen(texte2));
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 	dmp_diff_free(diff);
     
     long tailleFichier = ftell(fichier);
-    printf("\nTaile du premier fichier: %ld \n", tailleFichier);
+    printf("\nPosition pointeur du premier fichier: %ld \n", tailleFichier);
     
     printf("\nDiff terminé !\n");
     
